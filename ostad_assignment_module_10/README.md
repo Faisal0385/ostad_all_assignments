5b461dac0f7b8117f6a288ef7e2e14894b7139f6
Personal <faisaltez@gmail.com>
2023-07-17 13:46:58 +0600

    ostad assignment module 10

---
+++ ostad_assignment_module_10/README.md
@@ -0,0 +1,7 @@
+# ostad_assignment_module_10
+
+A new Flutter project.
+
+![img.png](img.png)
+![img_1.png](img_1.png)
+
---
+++ ostad_assignment_module_10/android/app/src/main/kotlin/com/example/ostad_assignment_module_10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ostad_assignment_module_10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
