a77d826d44bd3f4650650c0200d0318aa464de38
Personal <faisaltez@gmail.com>
2023-08-01 17:56:57 +0600

    change app icon and name

--- blog/lib/main.dart
+++ blog/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Blog App',
+      title: 'Tech Blog App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
       home: const SplashScreen(),
