beac4f0d3bc443ecbd69716e67c61ba8f751dfbd
Faisal0385 <faisaltez@gmail.com>
2023-09-28 18:37:49 +0600

    crafty bay ecom

---
+++ ostad_ecommerce/lib/application/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '/presentation/ui/screens/splash_screen.dart';
+
+class CraftyBay extends StatefulWidget {
+  const CraftyBay({super.key});
+
+  @override
+  State<CraftyBay> createState() => _CraftyBayState();
+}
+
+class _CraftyBayState extends State<CraftyBay> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
